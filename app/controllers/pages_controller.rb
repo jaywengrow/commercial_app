f9d2551415eddbb2c9e45dda3dcdb65ccc4a0f68
Jay Wengrow <jaywngrw@gmail.com>
2011-01-05 15:13:02 -0600

    Added root route and stylesheets from blueprint

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def home
+    @title = "Welcome to Commercial.ize!"
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 CommercialApp::Application.routes.draw do
+  root :to => "pages#home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

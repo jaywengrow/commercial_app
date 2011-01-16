c7936daffa8603c7451c27352560326eea8889e3
Jay Wengrow <jaywngrw@gmail.com>
2011-01-16 11:13:25 -0600

    fixed ajax issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     def logo
       image_tag("logo.png", :alt => "Commercial.ize logo", :class => "round")
     end
+    
+    def popular_posts
+    	@popular_posts = Post.popular.limit_5
+    end
 end
